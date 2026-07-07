141dcb4c49a39cac69d63b17fa72461b10c79837
Petri <p3l00ts@gmail.com>
2026-07-07 15:55:44 +0200

    bodies update

--- src/gravsim/2D_gravsim/bodies/gravsim_bodies.h
+++ src/gravsim/2D_gravsim/bodies/gravsim_bodies.h
@@ -1,5 +1,4 @@
 #pragma once
-// #include <GLFW/glfw3.h>
 #include <deque>
 #include <cmath>
 
@@ -20,6 +19,14 @@ struct Vec2 {
 
 struct Camera;
 
+enum class PlanetType {
+    Star = 0,
+    Terrestrial = 1,
+    GasGiant =2,
+    IceGiant = 3,
+    IcyMoon = 4
+};
+
 struct Body {
     Vec2 pos;
     Vec2 vel;
@@ -28,11 +35,20 @@ struct Body {
     float drawRadius;
     float r, g, b;
 
+    double rotationPeriod = 1.0e12;
+    double axialTilt = 0.0;
+    double rotationAngle = 0.0;
+    PlanetType type = PlanetType::IcyMoon;
+    float c2r = -1, c2g = -1, c2b = -1;
+    float c3r = -1, c3g = -1, c3b = -1;
+
     std::deque<Vec2> trail;
 
     Body(Vec2 pos, Vec2 vel, double mass,
          float drawRadius, float r, float g, float b);
 
+    void configureRotation (double periodSeconds, double tiltDeg, PlanetType t, float c2r_, float c2g_, float c2b_, float c3r_, float c3g_, float c3b_);
+    void updateRotation (double dtRealSeconds);
     void draw (const Camera& cam) const;
     void drawTrail(const Camera& cam) const;
 };
